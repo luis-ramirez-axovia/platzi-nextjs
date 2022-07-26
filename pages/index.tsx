ad301f061308db625e9f001ffe7ebb487642ee30
Luis Ramirez <luis.axovia@gmail.com>
2022-07-26 17:50:17 -0500

    Cambios statict y server SideProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,42 @@
 import React, { useState, useEffect } from 'react'
+import fetch from 'isomorphic-unfetch'
 import Layout from '@components/Layout/Layout'
 import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
 import ProductList from '@components/ProductList/ProductList'
 
-const HomePage = () => {
-  const [productList, setProductList] = useState<TProduct[]>([])
+// export const getServerSideProps = async () => {
+//   const response = await fetch('http://localhost:3000/api/avo')
+//   const { data }: TAPIAvoResponse = await response.json();
 
-  useEffect(() => {
-    window
-      .fetch('/api/avo')
-      .then((response) => response.json())
-      .then(({ data }: TAPIAvoResponse) => {
-        setProductList(data)
-      })
-  }, [])
+//   return {
+//     props: {
+//       productList: data,
+//     },
+//   }
+// }
+
+export const getStaticProps = async () => {
+  const response = await fetch('http://localhost:3000/api/avo')
+  const { data }: TAPIAvoResponse = await response.json();
+
+  return {
+    props: {
+      productList: data,
+    },
+  }
+}
+
+const HomePage = ({ productList }: { productList: TProduct[] }) => {
+  // const [productList, setProductList] = useState<TProduct[]>([])
+
+  // useEffect(() => {
+  //   window
+  //     .fetch('/api/avo')
+  //     .then((response) => response.json())
+  //     .then(({ data }: TAPIAvoResponse) => {
+  //       setProductList(data)
+  //     })
+  // }, [])
 
   return (
     <Layout>
--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,23 +1,61 @@
 import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import fetch from 'isomorphic-unfetch'
+import { GetStaticProps } from 'next'
 
 import Layout from '@components/Layout/Layout'
 import ProductSummary from '@components/ProductSummary/ProductSummary'
 
-const ProductPage = () => {
-  const { query } = useRouter()
-  const [product, setProduct] = useState<TProduct | null>(null)
+export const getStaticPaths = async () => {
+  const response = await fetch('http://localhost:3000/api/avo')
+  const { data }: TAPIAvoResponse = await response.json();
 
-  useEffect(() => {
-    if (query.id) {
-      fetch(`/api/avo/${query.id}`)
-        .then((response) => response.json())
-        .then((data: TProduct) => {
-          setProduct(data)
-        })
+  const paths = data.map(elm => ({
+    params: {
+      id: elm.id
     }
-  }, [query.id])
+  }))
+  return {
+    paths,
+    // incremental static generation
+    // 404 para todo lo que no esta en el array paths
+    fallback: false
+  }
+}
+
+export const getStaticProps: GetStaticProps= async ({ params }) => {
+  const id = params?.id as string;
+  const response = await fetch(`http://localhost:3000/api/avo/${id}`)
+  const data: TProduct = await response.json();
+
+  // if (query.id) {
+  //   fetch(`/api/avo/${query.id}`)
+  //     .then((response) => response.json())
+  //     .then((data: TProduct) => {
+  //       setProduct(data)
+  //     })
+  // }
+
+  return {
+    props: {
+      product: data,
+    },
+  }
+}
+
+const ProductPage = ({ product }: { product: TProduct}) => {
+  // const { query } = useRouter()
+  // const [product, setProduct] = useState<TProduct | null>(null)
+
+  // useEffect(() => {
+  //   if (query.id) {
+  //     fetch(`/api/avo/${query.id}`)
+  //       .then((response) => response.json())
+  //       .then((data: TProduct) => {
+  //         setProduct(data)
+  //       })
+  //   }
+  // }, [query.id])
 
   return (
     <Layout>
